8a9f438093d48a7421e2bb64138424fc4e5e384b
caotoanbkdt <caotoanbkdt@gmail.com>
2021-09-26 20:38:08 +0900

    bbasic complete

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,26 @@
-const btnEffect = document.querySelector('.nav-bar .btn');
+const btnEffects = document.querySelectorAll('.btn');
 
-const spanEffect = document.querySelector('.nav-bar .btn span');
-
-btnEffect.addEventListener('mouseover', () => {
-  spanEffect.style.animationName = 'effectButtonMoveIn';
+// scroll event
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.block .one').style.animationName = 'fade-in';
 });
 
-btnEffect.addEventListener('mouseout', () => {
-  spanEffect.style.animationName = 'effectButtonMoveOut';
+// header when scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    document.querySelector('header').style.top = '-50px';
+  } else {
+    document.querySelector('header').style.top = '0px';
+  }
+});
+
+//  button effect
+btnEffects.forEach((btnEffect) => {
+  btnEffect.addEventListener('mouseover', () => {
+    btnEffect.firstElementChild.style.animationName = 'effectButtonMoveIn';
+  });
+
+  btnEffect.addEventListener('mouseout', () => {
+    btnEffect.firstElementChild.style.animationName = 'effectButtonMoveOut';
+  });
 });
