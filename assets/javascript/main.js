abb53ce7ab04b035b1ab4f8dfc71244a8a703492
caotoanbkdt <caotoanbkdt@gmail.com>
2021-09-30 13:52:01 +0900

    completed  effect

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,25 @@ const btnEffects = document.querySelectorAll('.btn');
 
 // scroll event
 window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.block .one').style.animationName = 'fade-in';
+  document.querySelector('.block.one').style.animationName = 'fade-in';
+});
+
+// scroll and effect
+
+window.addEventListener('scroll', () => {
+  let scrollFade = (elFade, fadeName) => {
+    if (elFade.getBoundingClientRect()['top'] < innerHeight) {
+      elFade.style.animationName = fadeName;
+    }
+  };
+  Array.from(document.querySelectorAll('.block:not(.one)')).forEach((blockEl) =>
+    scrollFade(blockEl, 'fade-in')
+  );
+  Array.from(document.querySelectorAll('.block-imgs img')).forEach((imgEl) =>
+    scrollFade(imgEl, 'fade-in')
+  );
+  scrollFade(document.querySelector('.block1'), 'fade-in-b');
+  scrollFade(document.querySelector('.block5'), 'fade-in-b');
 });
 
 // header when scroll
